30040a5546c54892804f711f18fc262dd8de0ac2
Rod Johnson <johnsonroda@gmail.com>
2018-04-29 15:02:17 +1000

    Update K1Application.kt

--- src/main/kotlin/com/atomist/K1Application.kt
+++ src/main/kotlin/com/atomist/K1Application.kt
@@ -1,6 +1,6 @@
 package com.atomist
 
-import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.ApplicationRunner 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
